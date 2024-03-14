a1edb6115de800d7a437954b5ac4602272c60137
Bella Zhang <zhangzishan119@gmail.com>
2024-03-14 16:58:53 +1300

    Adjusted user routes and logic

--- controllers/users.js
+++ controllers/users.js
@@ -85,100 +85,168 @@ export const updatePassword = async (req, res) => {
   }
 };
 
-export const addRemoveDeleteCart = async (req, res) => {
+export const addToCart = async (req, res) => {
   try {
     const { id, productId } = req.params;
-    const { action } = req.body;
-    const user = await User.findById(id);
 
-    const index = user.cart.findIndex((item) => item.productId === productId);
+    // Find the user and update the cart
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id, "cart.productId": productId }, // Find user with given ID and matching cart item
+      { $inc: { "cart.$.quantity": 1 } }, // Increment quantity if cart item exists
+      { new: true } // Return the modified user document
+    );
 
-    switch (action) {
-      case "add":
-        if (index !== -1) {
-          user.cart[index].quantity++;
-        } else {
-          user.cart.push({ productId, quantity: 1 });
-        }
-        break;
-      case "remove":
-        if (index !== -1) {
-          if (user.cart[index].quantity > 1) {
-            user.cart[index].quantity--;
-          } else {
-            user.cart.splice(index, 1);
-          }
-        } else {
-          return res
-            .status(400)
-            .json({ message: "Item does not exist in the cart" });
-        }
-        break;
-      case "delete":
-        if (index !== -1) {
-          user.cart.splice(index, 1);
-        } else {
-          return res
-            .status(400)
-            .json({ message: "Item does not exist in the cart" });
-        }
-        break;
+    if (!updatedUser) {
+      // If user is not found or cart item does not exist, add a new item to cart
+      const newUser = await User.findByIdAndUpdate(
+        id,
+        { $push: { cart: { productId, quantity: 1 } } },
+        { new: true }
+      );
+      console.log("New item added to cart");
+      return res.status(200).json(newUser);
+    }
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const removeFromCart = async (req, res) => {
+  try {
+    const { id, productId } = req.params;
+
+    // Find the user and update the cart
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id, "cart.productId": productId }, // Find user with given ID and matching cart item
+      [
+        {
+          $set: {
+            "cart.$[element].quantity": {
+              $cond: {
+                if: { $eq: ["$cart.quantity", 1] }, // If quantity is 1
+                then: 0, // Set quantity to 0 (remove item from cart)
+                else: { $subtract: ["$cart.quantity", 1] }, // Decrease quantity by 1
+              },
+            },
+          },
+        },
+      ],
+      { arrayFilters: [{ "element.productId": productId }], new: true } // Return the modified user document
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User or cart item not found" });
     }
 
-    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
+
+export const deleteFromCart = async (req, res) => {
+  try {
+    const { id, productId } = req.params;
+
+    // Find the user and update the cart
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id },
+      { $pull: { cart: { productId: productId } } }, // Remove item from cart based on productId
+      { new: true } // Return the modified user document
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const addToWishlist = async (req, res) => {
+  try {
+    const { id, productId } = req.params;
+    const user = await User.findById(id);
+
+    // Find the user and update the wishlist
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id, "wishlist.productId": productId }, // Find user with given ID and matching wishlist item
+      { $inc: { "wishlist.$.quantity": 1 } }, // Increment quantity if wishlist item exists
+      { new: true } // Return the modified user document
+    );
+
+    if (!updatedUser) {
+      // If user is not found or wishlist item does not exist, add a new item to wishlist
+      const newUser = await User.findByIdAndUpdate(
+        id,
+        { $push: { wishlist: { productId, quantity: 1 } } },
+        { new: true }
+      );
+      console.log("New item added to wishlist");
+      return res.status(200).json(newUser);
+    }
+
+    console.log("Quantity incremented");
     res.status(200).json(updatedUser);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
 
-export const addRemoveDeleteWishlist = async (req, res) => {
+export const removeFromWishlist = async (req, res) => {
   try {
     const { id, productId } = req.params;
-    const { action } = req.body;
-    const user = await User.findById(id);
 
-    const index = user.wishlist.findIndex(
-      (item) => item.productId === productId
+    // Find the user and update the wishlist
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id, "wishlist.productId": productId }, // Find user with given ID and matching wishlist item
+      [
+        {
+          $set: {
+            "wishlist.$[element].quantity": {
+              $cond: {
+                if: { $eq: ["$wishlist.quantity", 1] }, // If quantity is 1
+                then: 0, // Set quantity to 0 (remove item from wishlist)
+                else: { $subtract: ["$wishlist.quantity", 1] }, // Decrease quantity by 1
+              },
+            },
+          },
+        },
+      ],
+      { arrayFilters: [{ "element.productId": productId }], new: true } // Return the modified user document
     );
 
-    switch (action) {
-      case "add":
-        if (index !== -1) {
-          user.wishlist[index].quantity++;
-        } else {
-          user.wishlist.push({ productId, quantity: 1 });
-        }
-        break;
-      case "remove":
-        if (index !== -1) {
-          if (user.wishlist[index].quantity > 1) {
-            user.wishlist[index].quantity--;
-          } else {
-            user.wishlist.splice(index, 1);
-          }
-        } else {
-          return res
-            .status(400)
-            .json({ message: "Item does not exist in the wishlist" });
-        }
-        break;
-      case "delete":
-        if (index !== -1) {
-          user.wishlist.splice(index, 1);
-        } else {
-          return res
-            .status(400)
-            .json({ message: "Item does not exist in the wishlist" });
-        }
-        break;
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User or wishlist item not found" });
     }
-
-    const updatedUser = await user.save();
 
     res.status(200).json(updatedUser);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+export const deleteFromWishlist = async (req, res) => {
+  try {
+    const { id, productId } = req.params;
+
+    // Find the user and update the wishlist
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id },
+      { $pull: { wishlist: { productId: productId } } }, // Remove item from wishlist based on productId
+      { new: true } // Return the modified user document
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,12 @@ import {
   getUser,
   getUserOrders,
   updateUser,
-  addRemoveDeleteCart,
-  addRemoveDeleteWishlist,
+  addToCart,
+  removeFromCart,
+  deleteFromCart,
+  addToWishlist,
+  removeFromWishlist,
+  deleteFromWishlist,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -16,7 +20,19 @@ router.get("/:_id/orders", verifyToken, getUserOrders);
 
 // Update
 router.patch("/:id", verifyToken, updateUser);
-router.patch("/:id/cart/:productId", verifyToken, addRemoveDeleteCart);
-router.patch("/:id/wishlist/:productId", verifyToken, addRemoveDeleteWishlist);
+router.patch("/:id/cart/add/:productId", verifyToken, addToCart);
+router.patch("/:id/cart/remove/:productId", verifyToken, removeFromCart);
+router.patch("/:id/cart/delete/:productId", verifyToken, deleteFromCart);
+router.patch("/:id/wishlist/add/:productId", verifyToken, addToWishlist);
+router.patch(
+  "/:id/wishlist/remove/:productId",
+  verifyToken,
+  removeFromWishlist
+);
+router.patch(
+  "/:id/wishlist/delete/:productId",
+  verifyToken,
+  deleteFromWishlist
+);
 
 export default router;
