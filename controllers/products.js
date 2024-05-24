43996e19bb327a023141442e6b8ba1740ffaaddc
Bella Zhang <zhangzishan119@gmail.com>
2024-05-24 14:40:12 +0800

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -3,29 +3,39 @@ import Product from "../models/Product.js";
 // Read
 export const getProducts = async (req, res) => {
   try {
-    const { category, search, price } = req.query;
+    const { categories, search, minPrice, maxPrice, sort } = req.query;
 
     let query = {};
-    if (category) {
-      query.categoryList = category;
+    if (categories) {
+      query.categoryList = { $in: categories.split(',') };
     }
 
     if (search) {
       query.$or = [
         { productName: { $regex: search, $options: "i" } },
-        { authorList: { $regex: search, $options: "i" } },
+        { authorNames: { $elemMatch: { $regex: search, $options: "i" } } },
       ];
     }
 
-    if (price) {
-      const [minPrice, maxPrice] = price.split("-");
-      query.productPrice = {
-        $gte: parseInt(minPrice),
-        $lte: parseInt(maxPrice),
-      };
+    if (minPrice || maxPrice) {
+      query.productPrice = {};
+      if (minPrice) {
+        query.productPrice.$gte = parseInt(minPrice);
+      }
+      if (maxPrice) {
+        query.productPrice.$lte = parseInt(maxPrice);
+      }
     }
 
-    const products = await Product.find(query);
+    let sortOptions = {};
+    if (sort) {
+      if (sort === "priceAsc") sortOptions.productPrice = 1;
+      if (sort === "priceDesc") sortOptions.productPrice = -1;
+      if (sort === "dateAsc") sortOptions.publishDate = 1;
+      if (sort === "dateDesc") sortOptions.publishDate = -1;
+    }
+
+    const products = await Product.find(query).sort(sortOptions);
 
     res.status(200).json(products);
   } catch (err) {
